64e3e5d7cdbc2c82140d80ac7cfa4c1dab11d325
Daniel Anorue Jr <danielanorue@gmail.com>
2024-06-20 10:08:17 -0500

    Add files via upload

---
+++ daniel_anorue_Lab0_MyFirstProgram.cpp
@@ -0,0 +1,19 @@
+/*
+Program Title: A Bit about Myself
+Author: Daniel Anorue
+Description: This program outputs a short paragraph
+             about myself.
+*/
+
+#include <iostream>
+
+using std::cout;
+
+int main()
+{
+    cout << "Greetings! My name is Daniel Anorue" << "\n";
+    cout << "I am A freshamn Computer Science Major at Southern Miss." << "\n";
+    cout << "My hobbies are playing basketball, photography, gaming and eating!" << "\n";
+    cout << "I also really love technology and my passion to understand it and be a contributor in the field is what motivated me to pursue Computer Science." << "\n";
+    cout << "I plan to make the most out of my education in Southern Miss to be a renowned proffesional in Technology. \n";
+}
